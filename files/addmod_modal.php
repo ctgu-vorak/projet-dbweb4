28176ba855522bc957938356b54e39866ac3a185
CLEMENT GUIMONNEAU <cleme@DESKTOP-H9482PL>
2021-04-26 23:54:17 +0200

    Possibilité de mettre à jour ses propres publications

--- files/addmod_modal.php
+++ files/addmod_modal.php
@@ -19,7 +19,7 @@
             <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
             <div class="w3-margin">
                 <label><b>Pseudo</b></label>
-                <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Pseudo" name="pseudo" maxlength="45" value="<?php echo $_SESSION['pseudo']?>" readonly required/>
+                <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Pseudo" name="pseudo" maxlength="45" value="<?php echo $_SESSION['pseudo']; ?>" readonly required/>
 
                 <label><b>Publication</b></label>
                 <textarea style="resize: none" class="w3-input w3-border w3-margin-bottom" name="content" placeholder="Contenu publication" maxlength="250" rows="auto" cols="auto" required></textarea>
@@ -75,22 +75,22 @@
             <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
                 <div class="w3-margin">
                     <label><b>Pseudo</b></label>
-                    <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Pseudo" name="pseudo" maxlength="45" value="<?php echo $_SESSION['pseudo']?>" readonly required/>
+                    <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Pseudo" name="mod_pseudo" maxlength="45" value="<?php echo $_SESSION['pseudo']; ?>" readonly required/>
 
                     <label for="chcat"><b>ID Publications</b></label>
-                    <select class="w3-select w3-border" name="chcat" id="chcat">
+                    <select class="w3-select w3-border" name="mod_id_pub" id="chcat" required>
                     <?php
                         if(isset($db)) {
                             $query = $db->prepare("SELECT id, contenu FROM publications WHERE auteur = ?");
                             $query->execute(array($_SESSION['id']));
                             $content = $query->fetchAll();
                             foreach($content as $key) {
-                                echo "<option value='".$key['id']."'>".$key['id']."</option>
+                                echo "<option value='{$key['id']}'>{$key['id']}</option>
                                       <br>";
                             }
                             echo "</select><br><br>
                             <label><b>Publication</b></label>
-                            <textarea style='resize: none' class='w3-input w3-border w3-margin-bottom' name='content' placeholder='Contenu publication' maxlength='250' rows='auto' cols='auto' required></textarea>
+                            <textarea style='resize: none' class='w3-input w3-border w3-margin-bottom' name='mod_content' placeholder='Contenu publication' maxlength='250' rows='auto' cols='auto' required></textarea>
                             ";
                         }
                     ?>
@@ -99,25 +99,25 @@
                     <table style="width: auto">
                         <tr  style="background-color: #ffffff">
                             <td class="w3-left">
-                                <input type="radio" name="categorie" value="divers" required/>
+                                <input type="radio" name="mod_categorie" value="divers" required/>
                                 <label>Divers</label><br>
                             </td>
                         </tr>
                         <tr style="background-color: #ffffff">
                             <td class="w3-left">
-                                <input type="radio" name="categorie" value="news" required/>
+                                <input type="radio" name="mod_categorie" value="news" required/>
                                 <label>News</label><br>
                             </td>
                         </tr>
                         <tr style="background-color: #ffffff">
                             <td class="w3-left">
-                                <input type="radio" name="categorie" value="sport" required/>
+                                <input type="radio" name="mod_categorie" value="sport" required/>
                                 <label>Sport</label><br>
                             </td>
                         </tr>
                         <tr style="background-color: #ffffff">
                             <td class="w3-left">
-                                <input type="radio" name="categorie" value="citation" required/>
+                                <input type="radio" name="mod_categorie" value="citation" required/>
                                 <label>Citation</label><br>
                             </td>
                         </tr>
@@ -126,7 +126,7 @@
                         <table class="w3-table w3-all-centered">
                             <tr style="height: auto">
                                 <th>
-                                    <button class="w3-button w3-round w3-block w3-green w3-section w3-hover-green" type="reset">Modifier</button>
+                                    <button class="w3-button w3-round w3-block w3-green w3-section w3-hover-green" type="submit">Modifier</button>
                                 </th>
                                 <th>
                                     <button class="w3-button w3-round w3-block w3-red w3-section w3-hover-red" type="reset">Recommencer</button>
--- index.php
+++ index.php
@@ -1,38 +1,39 @@
 <?php
     // Traitement du module de connexion
     require 'others_files/db_connect.php';
-    if(isset($_POST['pseudo_pseudo']) && isset($_POST['id_pseudo']) && isset($db)) {
-        require 'others_files/class_class.php';
-        $conn_pseudo = $_POST['pseudo_pseudo'];
-        $conn_id = (int)$_POST['id_pseudo'];
-        $query = $db->prepare("SELECT id, pseudo FROM utilisateurs WHERE id = ? AND pseudo = ?");
-        $query->execute(array($conn_id, $conn_pseudo));
-        $rslt = $query->fetchAll();
-        if(count($rslt) <= 0) {
-            echo "<script>alert('Pseudo ou ID incorrect. Retentez votre chance.')</script>";
-            header("Refresh:0");
-        } else {
+    if(isset($db)) {
+        if(isset($_POST['pseudo_pseudo'], $_POST['id_pseudo'])) {
+            require 'others_files/class_class.php';
+            $conn_pseudo = $_POST['pseudo_pseudo'];
+            $conn_id = (int)$_POST['id_pseudo'];
+            $query = $db->prepare("SELECT id, pseudo FROM utilisateurs WHERE id = ? AND pseudo = ?");
+            $query->execute(array($conn_id, $conn_pseudo));
+            $rslt = $query->fetchAll();
+            if(count($rslt) <= 0) {
+                echo "<script>alert('Pseudo ou ID incorrect. Retentez votre chance.')</script>";
+                header("Refresh:0");
+            } else {
+                session_start();
+                $_SESSION['pseudo'] = $rslt[0]['pseudo'];
+                $_SESSION['id'] = $rslt[0]['id'];
+                header("Refresh:0");
+                session_write_close();
+            }
+        }
+
+        // Traitement du module de déconnexion
+        if(isset($_POST['deconnexion'])) {
             session_start();
-            $_SESSION['pseudo'] = $rslt[0]['pseudo'];
-            $_SESSION['id'] = $rslt[0]['id'];
-            header("Refresh:0");
-            session_write_close();
+            if(isset($_SESSION)) {
+                unset($_SESSION['pseudo']);
+                unset($_SESSION['id']);
+                session_destroy();
+                header('Location: index.php');
+                exit;
+            }
+            session_abort();
         }
     }
-
-    // Traitement du module de déconnexion
-    if(isset($_POST['deconnexion'])) {
-        session_start();
-        if(isset($_SESSION)) {
-            unset($_SESSION['pseudo']);
-            unset($_SESSION['id']);
-            session_destroy();
-            header('Location: index.php');
-            exit;
-        }
-        session_abort();
-    }
-
 ?>
 
 <!doctype html>
@@ -72,19 +73,17 @@ require 'files/head.html';
     // Traitement des publications
     if(isset($db)) {
         // Traitement de l'ajout
-        if( isset($_POST['pseudo']) && isset($_POST['content']) && isset($_POST['categorie'])) {
+        if(isset($_POST['pseudo'], $_POST['content'], $_POST['categorie'])) {
             $pseudo = $_POST['pseudo'];
             $content = $_POST['content'];
             $cat = $_POST['categorie'];
 
             // Trouver l'auteur de la publication
-            $find_author = $db->prepare("SELECT id FROM utilisateurs WHERE pseudo = ?");
-            $find_author->execute(array($pseudo));
+            $find_author = $db->prepare("SELECT id FROM utilisateurs WHERE pseudo = ?")->execute(array($pseudo));
             $auteur = $find_author->fetchAll(PDO::FETCH_COLUMN,0);
 
             // Trouver la catégorie de la publication
-            $find_categorie = $db->prepare("SELECT id FROM categories WHERE categorie = ?");
-            $find_categorie->execute(array($cat));
+            $find_categorie = $db->prepare("SELECT id FROM categories WHERE categorie = ?")->execute(array($cat));
             $categorie = $find_categorie->fetchAll(PDO::FETCH_COLUMN,0);
 
             // Executer la requête d'insertion
@@ -93,9 +92,23 @@ require 'files/head.html';
         }
 
         // Traitement de la modification
+        if(isset($_POST['mod_pseudo'], $_POST['mod_content'], $_POST['mod_id_pub'] ,$_POST['mod_categorie'])) {
+            $m_pseudo = $_POST['mod_pseudo']; // varchar
+            $m_content = $_POST['mod_content']; // text
+            $m_id_pub = $_POST['mod_id_pub']; // int
+            $m_categorie = $_POST['mod_categorie']; // int
+
+            // Trouver la catégorie de la publication
+            $find_categorie = $db->prepare("SELECT id FROM categories WHERE categorie = ?");
+            $find_categorie->execute(array($m_categorie));
+            (int)$f_categorie = $find_categorie->fetchAll(PDO::FETCH_COLUMN,0);
+
+            $update_pub = $db->prepare("UPDATE publications SET contenu = ?, categorie = ? WHERE id = ?")->execute(array($m_content, $f_categorie[0], (int)$m_id_pub));
+
+            $url = "https://pedago.univ-avignon.fr/~uapv2001983/projet-dbweb4/index.php?idp=".$_SESSION['id'];
+            echo "<script>alert('Publication mise à jour.');</script><meta http-equiv='refresh' content='0;URL={$url}'>";
+        }
     }
-
-
 
 ?>
 
@@ -103,3 +116,6 @@ require 'files/head.html';
 
 </html>
 <!-- © Site imaginé et créé par Clément GUIMONNEAU (alias kaarov) pour le module DBWEB4 - Architecture Web et Bases de données de l'Université d'Avignon-->
+<!--
+
+-->
