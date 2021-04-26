0e0dfc7ba4a7d2ed541ad8e8e7f8dcbb70dcdcd1
CLEMENT GUIMONNEAU <cleme@DESKTOP-H9482PL>
2021-04-26 17:48:48 +0200

    Save

--- files/accueil.php
+++ files/accueil.php
@@ -9,7 +9,7 @@
         <div class='w3-center'>
             <h1>Bonjour ".$_SESSION['pseudo'].". Que souhaitez-vous voir ?</h1>
             
-            <a class='w3-button w3-hover-theme' href='?page_content=user_list'>Utilisateurs <br />
+            <a class='w3-button w3-hover-red' href='?page_content=user_list'>Utilisateurs <br />
                 <i class='fa fa-address-book' style='font-size:150%'></i>
             </a>
             <a class='w3-button w3-hover-theme' href='?page_content=publications'>Publications <br />
