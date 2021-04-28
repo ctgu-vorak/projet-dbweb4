b85cf629f0fcec3687cc5900e45cd0d5ccbcc20b
Vorak <69763603+ctgu-vorak@users.noreply.github.com>
2021-04-28 02:08:06 +0200

    Update error.php

--- error.php
+++ error.php
@@ -1,6 +1,11 @@
 <?php
-    // Traitement erreur 404
-    if($_GET['code'] == 404) {
-        header("Location: https://pedago.univ-avignon.fr/~uapv2001983/projet-dbweb4/index.php");
+    // Gestion des erreurs
+    switch($_GET['code']) {
+        case 404:
+            header("Location: https://pedago.univ-avignon.fr/~uapv2001983/projet-dbweb4/index.php");
+            break;
+        case 500:
+            echo "Erreur Interne du Serveur... Merci de patienter.";
+            break;
     }
 ?>
