5f298f1b7e17a0863bd8920f7646174a1bffdcd4
Vorak <69763603+ctgu-vorak@users.noreply.github.com>
2021-05-04 16:20:56 +0200

    Delete db connection

--- files/others/db_connect.php
+++ files/others/db_connect.php
@@ -2,9 +2,9 @@
 try {
     define('TYPE', 'pgsql');
     define('HOST', 'localhost');
-    define('BDD', 'etd');
-    define('USER', 'uapv2001983');
-    define('PASSWD', 'QCm8qv');
+    define('BDD', 'bdd');
+    define('USER', 'user');
+    define('PASSWD', '******');
 
     $db = new PDO(TYPE . ':host=' . HOST . ';dbname=' . BDD, USER, PASSWD);
     $db->exec("SET CHARACTER SET utf8");
